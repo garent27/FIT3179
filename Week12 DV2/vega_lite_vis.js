6edef506f2ea2771ffe409a40ea37d029953122b
garent27 <152369593+phab27@users.noreply.github.com>
2025-10-02 22:38:46 +0800

    Added interaction to heatmap

--- Week12 DV2/vega_lite_vis.js
+++ Week12 DV2/vega_lite_vis.js
@@ -1,3 +1,7 @@
+// Store references to the chart views
+let symbolMapView = null;
+let heatmapView = null;
+
 var vg_1 = "choropleth_map.vg.json";
 vegaEmbed("#choropleth_map", vg_1).then(function(result) {
 }).catch(console.error);
@@ -9,8 +13,25 @@ vegaEmbed('#bar_chart', vg_2).then(function(result) {
 
 var vg_3 = "symbol_map.vg.json";
 vegaEmbed('#symbol_map', vg_3).then(function(result) {
+    symbolMapView = result.view;
 }).catch(console.error);
 
-var vg_4 = "heatmap.vg.json";
+var vg_4 = "heatmap_new.vg.json";
 vegaEmbed('#heatmap', vg_4).then(function(result) {
+    heatmapView = result.view;
+    
+    // Add click listener to the heatmap
+    heatmapView.addEventListener('click', function(event, item) {
+        if (item && item.datum) {
+            const year = item.datum.year;
+            const continent = item.datum.continent;
+            
+            if (symbolMapView && year && continent) {
+                // Update the symbol map parameters
+                symbolMapView.signal('Year_selection', year);
+                symbolMapView.signal('center_to', continent);
+                symbolMapView.runAsync();
+            }
+        }
+    });
 }).catch(console.error);
