9c3aa941017be88fecbfb149d82f5351c0ddbf72
garent27 <gngo0003@student.monash.edu>
2025-10-06 15:29:21 +0800

    ETW week10

--- Week10 Applied/vega_lite_vis.js
+++ Week10 Applied/vega_lite_vis.js
@@ -1,3 +1,7 @@
-var vg_1 = "parallel_coordinates.vg.json";
-vegaEmbed("#parallel_coordinates", vg_1).then(function(result) {
+var multiline_spec = "multiline.vg.json";
+vegaEmbed("#multiline_chart", multiline_spec).then(function(result) {
+}).catch(console.error);
+
+var bubble_spec = "bubble_timeline.vg.json";
+vegaEmbed("#bubble_timeline", bubble_spec).then(function(result) {
 }).catch(console.error);
